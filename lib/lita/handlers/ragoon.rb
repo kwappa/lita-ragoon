1af7d4e569819e62f4b4a994611c8e3943fe1a25
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-11-22 14:25:51 +0900

    :sunrise_over_mountains: implement handler method

--- lib/lita/handlers/ragoon.rb
+++ lib/lita/handlers/ragoon.rb
@@ -1,7 +1,12 @@
 module Lita
   module Handlers
     class Ragoon < Handler
-      # insert handler code here
+      route(/\Aragoon.*today\Z/i, :today)
+
+      def today(request)
+        events = ::Ragoon::Services::Schedule.new.schedule_get_events
+        request.reply(render_template('today', events: events))
+      end
 
       Lita.register_handler(self)
     end
