b504919a719c83e35e230855709b1cfb950f89e3
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-11-26 21:14:46 +0900

    :muscle: enable to get schedule of tomorrow

--- lib/lita/handlers/ragoon.rb
+++ lib/lita/handlers/ragoon.rb
@@ -9,8 +9,21 @@ module Lita
         }
       )
 
+      route(
+        /\Aragoon.*tomorrow\Z/i, :tomorrow,
+        command: true,
+        help: {
+          t('help.tomorrow.syntax') => t('help.tomorrow.desc'),
+        }
+      )
+
       def today(request)
         events = ::Ragoon::Services::Schedule.new.schedule_get_events
+        request.reply(render_template('today', events: events))
+      end
+
+      def tomorrow(request)
+        events = ::Ragoon::Services::Schedule.new.schedule_get_events(::Ragoon::Services.start_and_end(Date.today + 1))
         request.reply(render_template('today', events: events))
       end
 
