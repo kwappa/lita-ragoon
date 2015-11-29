e433653e5f4f9ced865c918bcb56ffa7c4c5939d
SHIOYA, Hiromu <kwappa.856@gmail.com>
2015-11-30 02:48:45 +0900

    :construction: improve format to diplay events

--- lib/lita/handlers/ragoon.rb
+++ lib/lita/handlers/ragoon.rb
@@ -22,15 +22,63 @@ module Lita
 
       def today(request)
         events = ::Ragoon::Services::Schedule.new.schedule_get_events
-        request.reply(render_template('events', events: events))
+        request.reply(
+          render_template(
+            'events',
+            events: format_events(events, request.private_message?)
+          )
+        )
       end
 
       def tomorrow(request)
         events = ::Ragoon::Services::Schedule.new.schedule_get_events(::Ragoon::Services.start_and_end(Date.today + 1))
-        request.reply(render_template('events', events: events))
+        request.reply(
+          render_template(
+            'events',
+            events: format_events(events, request.private_message?)
+          )
+        )
       end
 
       Lita.register_handler(self)
+
+      private
+
+      def format_events(events, private)
+        events.map { |event| format_event(event, private) }
+      end
+
+      def format_event(event, private)
+        plan = event[:plan].to_s != '' ? "【#{event[:plan].strip}】" : ''
+        period = if event[:allday]
+                   '終日'
+                 else
+                   "#{format_time(event[:start_at])}〜#{format_time(event[:end_at])}"
+                 end
+        if !private && event[:private]
+          title = '予定あり'
+          facilities = ''
+        else
+          title = event[:title]
+          facilities = event[:facilities].join(', ')
+        end
+
+        {
+          plan:       plan,
+          period:     period,
+          title:      title,
+          facilities: facilities,
+          private:    event[:private],
+        }
+      end
+
+      def format_time(time)
+        if time.to_s == ''
+          ''
+        else
+          Time.parse(time).localtime.strftime('%R')
+        end
+      end
     end
   end
 end
